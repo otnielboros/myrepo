b20c2e452b7ec8aafc3e342c67feab351f0d3f9e
florinotnielboros <otnielboros@yahoo.com>
2019-01-15 16:44:16 +0200

    Mobile Apps

--- MobileApps/Lab2React/Task/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TaskStore from './TaskStore.js';
-import {TaskList} from './TaskList.js';
-
-export default () => (
-  <TaskStore>
-    <TaskList/>
-  </TaskStore>
-);
--- MobileApps/Lab2React/core/api.js
+++
@@ -1,7 +0,0 @@
-export const apiUrl = '192.168.0.100:8080';
-export const httpApiUrl = `http://${apiUrl}`;
-export const wsApiUrl = `ws://${apiUrl}`;
-export const headers = {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json'
-};
